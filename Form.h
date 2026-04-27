9ca362fd6e1f6136272a6ed20142efdb51bcdc4a
YCaTGameRT <ycatgamert@gmail.com>
2026-04-27 12:25:00 +0500

    Добавьте файлы проекта.

---
+++ Form.h
@@ -0,0 +1,54 @@
+#pragma once
+#ifndef _FORM_H_
+#define _FORM_H_
+
+#include <string>
+#include <iostream>
+
+class Form {
+public:
+	//enum class в отличии от enum не доступен без содержащего его класса
+	enum class Shapes {
+		circle,
+		triangle,
+		square,
+		star
+	};
+	
+	Form(Shapes shape) {
+		this->shape = shape;
+		switch (shape) {
+		case Shapes::circle:
+			this->name = "crircle";
+			break;
+		case Shapes::triangle:
+			this->name = "triangle";
+			break;
+		case Shapes::square:
+			this->name = "square";
+			break;
+		case Shapes::star:
+			this->name = "star";
+			break;
+		}
+	}
+
+	std::string GetName() const {
+		return name; 
+	}
+	Shapes GetShape() const {
+		return shape;
+	}
+
+	virtual bool operator==(const Form& rso) const { 
+		return shape == rso.shape;
+	}
+	virtual bool IsEqual(const Form& rso) const {
+		return *this == rso;
+	}
+private:
+	Shapes shape;
+	std::string name;
+};
+
+#endif //!_FORM_H_
---
+++ Frames.h
@@ -0,0 +1,30 @@
+#pragma once
+#ifndef _FRAMES_H_
+#define _FRAMES_H_
+
+#include <string>
+#include <iostream>
+#include <memory>
+
+#include "Form.h"
+#include "Toys.h"
+
+class Frames {
+public:
+	Frames(const Form& form) : form(std::make_unique<Form>(form)) {}
+
+	bool IsSame(const Frames& other) {
+		return this == &other;
+	}
+	const Form& GetForm() const {
+		return *form;
+	}
+	bool IsPassable(const Toys& toy) const {
+		return form->IsEqual(toy.GetForm());
+	}
+	//void Collect(const Toys& toy); уместен для объекта который сможет хранить в себе игрушки
+private:
+	std::unique_ptr<Form> form;
+};
+
+#endif //!_FRAMES_H_
